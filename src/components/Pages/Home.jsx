7069086221c68c25a2146aa1b037493261be8cca
beenow011 <abhinavnb11@gmail.com>
2023-12-19 20:27:03 +0530

    home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { CardTemplate } from "../CardTemplate";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 export const Home = () => {
+  const userStatus = useSelector((state) => state.auth.status);
+
   const tweetSelector = useSelector((state) => state.tweets.allTweets);
   const [tweets, setTweets] = useState(tweetSelector);
 
-  return (
+  return userStatus ? (
     <div>
       <h1 className="text-3xl font-bold text-start p-5 text-white">
         Top tweets
@@ -23,5 +26,13 @@ export const Home = () => {
         ))}
       </div>
     </div>
+  ) : (
+    <>
+      <Link to="/login">
+        <h1 className="text-white text-3xl font-mono font-bold underline m-6 w-fit mx-auto bg-pink-400 rounded-lg hover:bg-white hover:text-pink-300 p-5">
+          Login to show your profile
+        </h1>
+      </Link>
+    </>
   );
 };
